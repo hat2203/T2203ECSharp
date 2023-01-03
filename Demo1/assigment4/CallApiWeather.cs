e7f783f1bed241a9d28a5215ad7646ed1a639a4c
hat2203 <hantth2203018@fpt.edu.vn>
2023-01-03 20:42:13 +0700

    btvn

---
+++ Demo1/assigment4/CallApiWeather.cs
@@ -0,0 +1,33 @@
+﻿using Demo1.session5;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.assigment4
+{
+    public class CallApiWeather
+    {
+        public async Task<ApiWeather> FetchData()
+        {
+
+            string url = "https://dummyjson.com/products/1";
+            HttpClient client = new HttpClient();
+            var rs = await client.GetAsync(url);
+            if (rs.StatusCode == HttpStatusCode.OK)
+            {
+                string responseText = await rs.Content.ReadAsStringAsync();
+                ApiWeather a = JsonConvert.DeserializeObject<ApiWeather>(responseText);
+                return a;
+
+            }
+
+            return null;
+        }
+
+    }
+}
