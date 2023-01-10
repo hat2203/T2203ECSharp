278590036c7a7076ea3089c34ca9ec9c8f4be78e
hat2203 <hantth2203018@fpt.edu.vn>
2023-01-10 19:28:35 +0700

    Exam

---
+++ Demo1/practice exam/Process.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo1.practice_exam
+{
+    internal class Process
+    {
+        public double Radius { get; set; }
+        public double Height { get; set; }
+
+        public void Input()
+        {
+            Console.WriteLine("Enter the dimenstions of the cylinder");
+            Console.Write("Radius:");
+            this.Radius = double.Parse(Console.ReadLine());
+            Console.Write("Height:");
+            this.Height = double.Parse(Console.ReadLine());
+        }
+
+        public double Basarea()
+        {
+             
+            return Basarea();
+        }
+    }
+}
